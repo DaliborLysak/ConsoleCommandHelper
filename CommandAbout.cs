5a1ce600b91d82e6a259b3fba117d10fd4dd5f98
DaliborLysak <lysak@provys.com>
2020-08-14 12:17:48 +0200

    #1 Add unit test for unprocessed command

--- CommandAbout.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ConsoleCommandHelper
-{
-    [Command(Names = new string[] { "ABOUT", "-ABOUT", "--ABOUT" }, FileName = "About.txt")]
-    internal class CommandAbout : CommonCommand
-    {
-    }
-}
