8d8450bc133343ba25ddc85a26882872e636efe6
DaliborLysak <lysak@provys.com>
2020-08-17 10:37:12 +0200

    + known command output test

--- ConsoleCommandHelper.Tests/ConsoleCommandHelperUnitTest.cs
+++ ConsoleCommandHelper.Tests/ConsoleCommandHelperUnitTest.cs
@@ -1,14 +1,21 @@
 using Xunit;
 using ConsoleCommandHelper;
+using System;
 
 namespace ConsoleCommandHelper.UnitTests
 {
     public class ConsoleCommandHelperUnitTest
     {
         [Fact]
-        public void UnknownCommandTest()
+        public void UnknownCommandOutputTest()
         {
             Assert.Equal("UnknownCommand", CommandService.Execute("UnknownCommand"));
+        }
+
+        [Fact]
+        public void KnownCommandOutputTest()
+        {
+            Assert.Equal(String.Empty, CommandService.Execute("--HELP"));
         }
     }
 }
